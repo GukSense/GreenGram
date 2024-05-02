35d87fcf3ea7352235ed425f6a770e6672188713
Administrator <redraib05@gmail.com>
2024-05-02 13:15:37 +0900

    test

---
+++ src/main/java/com/green/greengramver1/common/model/ResultDto.java
@@ -0,0 +1,15 @@
+package com.green.greengramver1.common.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@Data
+public class ResultDto<T> {
+    private HttpStatus status;
+    private String msg;
+    private T result;
+
+
+}
